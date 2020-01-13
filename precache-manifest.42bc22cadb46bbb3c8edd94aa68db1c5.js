9663e9f6607040e7603edc22494785c07d8e77ca
akankshafb <akankshawebdeveloper@gmail.com>
2020-01-14 00:28:50 +0530

    Updates

---
+++ precache-manifest.42bc22cadb46bbb3c8edd94aa68db1c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9eddf1e4a43e33b90fc6034f7ceafa",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "c3605111a52045b28326",
+    "url": "/hello/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8264654eca1ed0bd6dd4",
+    "url": "/hello/static/css/main.e5c996c2.chunk.css"
+  },
+  {
+    "revision": "c3605111a52045b28326",
+    "url": "/hello/static/js/2.6f42e0b2.chunk.js"
+  },
+  {
+    "revision": "8264654eca1ed0bd6dd4",
+    "url": "/hello/static/js/main.14512246.chunk.js"
+  },
+  {
+    "revision": "74d9a52fd3337a20be28",
+    "url": "/hello/static/js/runtime-main.013379f0.js"
+  },
+  {
+    "revision": "fa6be6f9b103f33ced9437b2c8838c43",
+    "url": "/hello/static/media/cart.fa6be6f9.png"
+  }
+]);
